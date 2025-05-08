684b76a2950d97c66f4e3f5c4b9709ba4cfa3f54
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-05-08 18:11:53 -0300

    primeira aula do curso de java

---
+++ aula1-variaveis/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void  main(String[] args) {
+        //<tipo> <nome> = <valor inicial(opicional)>
+
+        int idade =25; //inteiro
+        double aultura = 1.68; //decimal
+        char sexo = 'F'; // um caracter - unicode
+
+
+        // tipos primitivos em java
+        // | Tipo       | Tamanho (em bytes) | Valores                                   |
+        // |------------|---------------------|-------------------------------------------|
+        // | byte       | 1                   | -128 a 127                                |
+        // | short      | 2                   | -32.768 a 32.767                          |
+        // | int        | 4                   | -2.147.483.648 a 2.147.483.647           |
+        // | long       | 8                   | -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807 |
+        // | float      | 4                   | ±1.4E-45 a ±3.4028235E38                  |
+        // | double     | 8                   | ±4.9E-324 a ±1.7976931348623157E308      |
+        // | char       | 2                   | 0 a 65.535 (representa caracteres Unicode) |
+        // | boolean    | 1 (não definido oficialmente) | true ou false                      |
+
+    }
+
+}

9561035d8cee8aeb8e9dd44c6f3531caf56a0fcf
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-05-16 14:59:05 -0300

    aula de ecpreções comparativas

---
+++ aula1-variaveis/Aula7ExprecoesComparativas.java
@@ -0,0 +1,19 @@
+public class Aula7ExprecoesComparativas {
+    public static void main(String[] args){
+        // > maior
+        // < menor
+        // >= maior ou igual
+        // <= menor ou igual
+        // == igual
+        // != diferente
+
+        // exemplos
+
+        int x;
+        x = 5;
+        System.out.println( x > 0 ); // true
+        System.out.println( x == 3 ); // false
+        System.out.println( 10 <= 30 ); // true
+        System.out.println( x != 2 ); // true
+    }
+}

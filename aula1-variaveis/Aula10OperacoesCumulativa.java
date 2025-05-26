bd9322574d1e18747ca75c3964bc4cc00a0743a2
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-05-26 13:56:01 -0300

    aula sobre operações de atribuiçoes cumulativas

---
+++ aula1-variaveis/Aula10OperacoesCumulativa.java
@@ -0,0 +1,9 @@
+public class Aula10OperacoesCumulativa {
+    static public void main(String[] args){
+        //a += b | a = a + b
+        //a -= b | a = a - b
+        //a *= b | a = a * b
+        //a /= b | a = a / b
+        //a %= b | a = a % b
+    }
+}

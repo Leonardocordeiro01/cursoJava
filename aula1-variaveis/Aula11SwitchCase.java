874b97e7c47cb0262055193c8abb845419f3ce90
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-05-26 14:10:53 -0300

    aula sobre switch-case

---
+++ aula1-variaveis/Aula11SwitchCase.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Aula11SwitchCase {
+    static public void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda";
+                break;
+            case 3:
+                dia = "terça";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+            default:
+                dia = "valor invalido";
+                break;
+        }
+
+        System.out.println("dia da semana "+ dia);
+
+        sc.close();
+    }
+}

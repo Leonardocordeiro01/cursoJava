cef4cf786a547e50d47d7fe579559c89cd9c9f8b
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-06-17 16:27:29 -0300

    resoluçao de exercicio senem ter estudado o modulo de POO

--- java-poo/src/Main.java
+++ java-poo/src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+// MINHA RESOLUÇÃO DO PROBLEMA ANTES DE ESTUDAR O MODULO DE ORIENTAÇÃO A OBJETOS
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        double areaX, areaY;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("enter the measures of tringle X: ");
+        areaX = readValues();
+        System.out.printf("Triangle X area %.2f %n ", areaX);
+
+        System.out.println("enter the measures of tringle Y: ");
+        areaY = readValues();
+        System.out.printf("Triangle Y area %.2f %n ", areaY);
+
+        if (areaX > areaY){
+            System.out.println("Larger area: X");
+        } else if (areaX < areaY) {
+            System.out.println("Larger area: Y");
+        } else {
+            System.out.println("Equal areas");
         }
+
+        sc.close();
+    }
+
+    public static double readValues() {
+        Scanner sc = new Scanner(System.in);
+        double area, a, b, c, p;
+
+        System.out.println("enter the value of a: ");
+        a = sc.nextDouble();
+        System.out.println("enter the value of b: ");
+        b = sc.nextDouble();
+        System.out.println("enter the value of c: ");
+        c = sc.nextDouble();
+
+        p = (a+b+c)/2;
+
+        return area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
     }
 }

04c6e9bd32bd5e83208fb5d5309757500ce00220
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-07-01 12:08:23 -0300

    Aula sobre construtores

---
+++ java-poo/aula2Construtores/src/src/application/Main.java
@@ -0,0 +1,35 @@
+package src.application;
+
+
+import src.entities.Product;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("name of product: ");
+        String name = sc.nextLine();
+        System.out.println("price of product: ");
+        double price = sc.nextDouble();
+        System.out.println("quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println("Product data: " + product);
+
+        System.out.println("Enter the number of product to be added in stock ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+        System.out.println("Product data: " + product);
+
+        System.out.println("Enter the number of product be remove for stock ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println("Product data: " + product);
+
+        sc.close();
+    }
+}
---
+++ java-poo/aula2Construtores/src/src/entities/Product.java
@@ -0,0 +1,36 @@
+package src.entities;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product (String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double stock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                +", "
+                +String.format("%.2f ", price)
+                +", "
+                +quantity
+                +"total stock: "
+                +String.format("%.2f ", stock());
+    }
+}

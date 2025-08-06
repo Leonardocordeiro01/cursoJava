e2c3110109db5c52b8ad6c75596a08fa68a44219
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-08-05 22:58:11 -0300

    exercicio 1 de vetores

---
+++ java-poo/Exercicio06/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+        int[] vet = new int[n];
+
+        for(int i = 0; i < vet.length; i++){
+            sc.nextLine();
+            System.out.println("digite um numero: ");
+            vet[i] = sc.nextInt();
+        }
+
+        System.out.println("Numeros negativos: ");
+        for (int i = 0; i < vet.length; i++){
+            if (vet[i] < 0){
+                System.out.println(vet[i]);
+            }
+        }
+
+        sc.close();
+    }
+}

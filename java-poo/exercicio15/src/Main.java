b25459c2c8c259a8946a50b20cf7b162cf759307
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-08-19 18:52:20 -0300

    exercicio 15 aprovados

--- java-poo/exercicio15/src/Main.java
+++ java-poo/exercicio15/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        String[] nome = new String[n];
+        double[] primeiraNota = new double[n];
+        double[] segundaNota = new double[n];
+
+        for (int i=0; i < n; i++){
+            sc.nextLine();
+            System.out.printf("Digite o nome, primeira e segunda nota do %do aluno: %n", i+1);
+            nome[i] = sc.nextLine();
+            primeiraNota[i] = sc.nextDouble();
+            segundaNota[i] = sc.nextDouble();
+        }
+
+        double[] medias = new double[n];
+        System.out.println("Alunos aprovados: ");
+        for (int i=0; i < n; i++){
+            medias[i] = (primeiraNota[i] + segundaNota[i]) / 2;
+            System.out.println(medias[i]);
+            if (medias[i] >= 6.0){
+                System.out.println(nome[i]);
+            }
+        }
+
+        sc.close();
     }
 }
